2f59b7341304f4dec3449e6fb3317fc2d2afd065
ASHISH GAUTAM <ashish.gautam.254681@gmail.com>
2026-07-16 23:59:14 +0530

    Delete src directory
    this file is not in folder

--- src/main/java/in/sp/datajpa/main/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package in.sp.datajpa.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.sp.datajpa.main.std.Student;
-
-public interface StudentRepository extends JpaRepository<Student,Long > {
-
-}
